5fe52090e446a9c85f225525d7e66b910ac519ec
William David Ferreira da Silva Lopes <69218953+William-dev1992@users.noreply.github.com>
2020-12-02 10:20:28 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,26 @@
 let game = {
 
-
     lockMode: false,
     firstCard: null,
     secondCard: null,
 
+    techs: ['bootstrap',
+        'css',
+        'electron',
+        'firebase',
+        'html',
+        'javascript',
+        'jquery',
+        'mongo',
+        'node',
+        'react'],
+
+    cards: null,
+
     setCard: function (id) {
 
-        let card = this.cards.filter(card => card.id === id) [0];
-
+        let card = this.cards.filter(card => card.id === id)[0];
+        console.log(card);
         if (card.flipped || this.lockMode) {
             return false;
         }
@@ -16,17 +28,18 @@ let game = {
         if (!this.firstCard) {
             this.firstCard = card;
             this.firstCard.flipped = true;
-            return false;
+            return true;
         } else {
             this.secondCard = card;
             this.secondCard.flipped = true;
             this.lockMode = true;
             return true;
         }
+
     },
 
     checkMatch: function () {
-        if(!this.firstCard || !this.secondCard){
+        if (!this.firstCard || !this.secondCard) {
             return false;
         }
         return this.firstCard.icon === this.secondCard.icon;
@@ -37,29 +50,20 @@ let game = {
         this.secondCard = null;
         this.lockMode = false;
     },
-
-    unflipCards: function() {
+    unflipCards() {
         this.firstCard.flipped = false;
         this.secondCard.flipped = false;
         this.clearCards();
     },
 
-    checkGameOver: function() {
-        return this.cards.filter(card => !card.flipped).length == 0;
+    checkGameOver() {
+
+        return this.cards.filter(card => !card.flipped).length === 0;
     },
 
-    techs: ['bootstrap',
-            'css',
-            'electron',
-            'firebase',
-            'html',
-            'javascript',
-            'jquery',
-            'mongo',
-            'node',
-            'react'],
 
-    cards: null,
+
+
 
     createCardsFromTechs: function () {
 
@@ -104,7 +108,5 @@ let game = {
         }
 
     }
-
-
 
 }
